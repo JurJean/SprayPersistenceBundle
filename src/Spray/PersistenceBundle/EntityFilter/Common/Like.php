113eeaa8f4534aded0a5c03d33533adc5cdd0b64
Thul <thul@arienai.com>
2013-09-26 14:47:04 +0000

    Added Like filter

---
+++ src/Spray/PersistenceBundle/EntityFilter/Common/Like.php
@@ -0,0 +1,28 @@
+<?php
+namespace Spray\PersistenceBundle\EntityFilter\Common;
+
+use Doctrine\ORM\QueryBuilder;
+use Spray\PersistenceBundle\EntityFilter\EntityFilterInterface;
+
+class Like extends AbstractLikeFilter implements EntityFilterInterface
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function filter(QueryBuilder $qb)
+    {
+        foreach($this->arguments as $property => $value){
+            $qb->andWhere(sprintf('%s.%s LIKE :incoming_%2$s', $qb->getRootAliases()[0], $property));
+            $qb->setParameter('incoming_' . $property, '%' . $value . '%');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'like';
+    }
+}
