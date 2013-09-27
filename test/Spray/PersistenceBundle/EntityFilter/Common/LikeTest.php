b0ce3775019239d79e37b7574e7dcd2159643dd9
Thul <thul@arienai.com>
2013-09-27 09:57:29 +0000

    Added unittest

---
+++ test/Spray/PersistenceBundle/EntityFilter/Common/LikeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Spray\PersistenceBundle\EntityFilter\Common;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * AscendingTest
+ */
+class LikeTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->queryBuilder = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testFilter()
+    {
+        $count = 1;
+
+        $this->queryBuilder->expects($this->at($count))
+            ->method('getRootAliases')
+            ->will($this->returnValue(array('a')));
+        $this->queryBuilder->expects($this->at($count))
+            ->method('andWhere')
+            ->with($this->equalTo('a.foo LIKE :incoming_foo'));
+        $this->queryBuilder->expects($this->at($count++))
+            ->method('setParameter')
+            ->with(
+                $this->equalTo('incoming_foo'),
+                $this->equalTo('%bar%')
+            );
+
+        $filter = new Like(array(
+            'foo' => 'bar',
+            'bar' => 'baz',
+        ));
+        $filter->filter($this->queryBuilder);
+    }
+}
